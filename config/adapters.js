8745c708d2b294965c9695e83140869d3ffd42b1
Will Ricketts <wricketts@procur.com>
2014-06-05 19:56:03 -0400

    Added config data from procur-core

--- config/adapters.js
+++ config/adapters.js
@@ -1,8 +1,8 @@
 /**
  * Global adapter config
- * 
+ *
  * The `adapters` configuration object lets you create different global "saved settings"
- * that you can mix and match in your models.  The `default` option indicates which 
+ * that you can mix and match in your models.  The `default` option indicates which
  * "saved setting" should be used if a model doesn't have an adapter specified.
  *
  * Keep in mind that options you define directly in your model definitions
@@ -12,28 +12,53 @@
  * http://sailsjs.org/#documentation
  */
 
-module.exports.adapters = {
+module.exports = {
 
-  // If you leave the adapter config unspecified 
-  // in a model definition, 'default' will be used.
-  'default': 'disk',
+  adapters:{
+
+  'default': 'mongo',
+
+    mongo: {
+      module: 'sails-mongo',
+      url: process.env.DB_URL,
+      schema: true
+    },
+
+  }
+    /*disk: {
+      module: 'sails-disk'
+    }
+
+  environment: 'development',
+  adapters:{
+
+  'default': 'mongo',
+
+    mongo: {
+      module: 'sails-mongo',
+      host: 'localhost',
+      port: '27017',
+      database: 'procur'
+    },
+    disk: {
+      module: 'sails-disk'
+    }
+  }
 
   // Persistent adapter for DEVELOPMENT ONLY
   // (data is preserved when the server shuts down)
-  disk: {
-    module: 'sails-disk'
-  },
+
 
   // MySQL is the world's most popular relational database.
   // Learn more: http://en.wikipedia.org/wiki/MySQL
-  myLocalMySQLDatabase: {
+  /*myLocalMySQLDatabase: {
 
     module: 'sails-mysql',
     host: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
     user: 'YOUR_MYSQL_USER',
     // Psst.. You can put your password in config/local.js instead
     // so you don't inadvertently push it up if you're using version control
-    password: 'YOUR_MYSQL_PASSWORD', 
+    password: 'YOUR_MYSQL_PASSWORD',
     database: 'YOUR_MYSQL_DB'
-  }
+  }*/
 };
--- config/session.js
+++ config/session.js
@@ -1,7 +1,7 @@
 /**
  * Session
- * 
- * Sails session integration leans heavily on the great work already done by Express, but also unifies 
+ *
+ * Sails session integration leans heavily on the great work already done by Express, but also unifies
  * Socket.io with the Connect session store. It uses Connect's cookie parser to normalize configuration
  * differences between Express and Socket.io and hooks into Sails' middleware interpreter to allow you
  * to access and auto-save to `req.session` with Socket.io the same way you would with Express.
@@ -14,23 +14,38 @@ module.exports.session = {
 
   // Session secret is automatically generated when your new app is created
   // Replace at your own risk in production-- you will invalidate the cookies of your users,
-  // forcing them to log in again. 
-  secret: '704781b9970fcb8a3f3536e51447ac4e'
+  // forcing them to log in again.
+
+
+
+
+  ////////// DO NOT CHANGE!!! //////////
+  secret: '8666c1f82163ab798e9792f1911cadcb',
 
 
   // In production, uncomment the following lines to set up a shared redis session store
   // that can be shared across multiple Sails.js servers
-  // adapter: 'redis',
+  adapter: 'redis',
   //
   // The following values are optional, if no options are set a redis instance running
   // on localhost is expected.
   // Read more about options at: https://github.com/visionmedia/connect-redis
   //
-  // host: 'localhost',
-  // port: 6379,
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
   // ttl: <redis session TTL in seconds>,
-  // db: 0,
-  // pass: <redis auth password>
+  db: process.env.REDIS_DB,
+  pass: process.env.REDIS_PASS
+
+  /*
+
+  host: 'angelfish.redistogo.com',
+  port: 9809,
+  // ttl: <redis session TTL in seconds>,
+  db: 'redistogo',
+  pass: '0d5b806697a93b4cd6f1f66bd4b24c18'
+
+  */
   // prefix: 'sess:'
 
 
