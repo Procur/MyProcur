2381badd3f3a5a82a9d67209a45ff543fa218a5b
Will Ricketts <wricketts@procur.com>
2014-06-10 12:46:13 -0400

    removed ssl from procur-core redirect

--- api/policies/isAuthenticated.js
+++ api/policies/isAuthenticated.js
@@ -17,6 +17,6 @@ module.exports = function(req, res, next) {
 
   // User is not allowed
   // (default res.forbidden() behavior can be overridden in `config/403.js`)
-  return res.redirect('https://localhost:1337');
+  return res.redirect('http://localhost:1337');
   //return res.forbidden('You are not permitted to perform this action.');
 };
