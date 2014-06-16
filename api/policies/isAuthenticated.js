cd11e5638f98d408df3a4b911d87c37fb7ff4765
Will Ricketts <will@willricketts.com>
2014-06-16 10:46:46 -0400

    debugging

--- api/policies/isAuthenticated.js
+++ api/policies/isAuthenticated.js
@@ -11,18 +11,21 @@ module.exports = function(req, res, next) {
   console.log(req.session);
   // User is allowed, proceed to the next policy,
   // or if this is the last policy, the controller
-  if (req.session.authenticated) {
+  if (req.session.authenticated == true)  {
     return next();
   }
+  else {
+    // User is not allowed
+    // (default res.forbidden() behavior can be overridden in `config/403.js`)
+    if(process.env.NODE_ENV == 'development'){
+      return res.redirect('http://localhost:1337');
+    }
+    else{
+      return res.redirect('http://procur-core-staging.herokuapp.com');
+    }
+  }
 
-  // User is not allowed
-  // (default res.forbidden() behavior can be overridden in `config/403.js`)
-  if(process.env.NODE_ENV == 'development'){
-    return res.redirect('http://localhost:1337');
-  }
-  else{
-    return res.redirect('http://procur-core-staging.herokuapp.com');
-  }
+
 
   //return res.forbidden('You are not permitted to perform this action.');
 };
